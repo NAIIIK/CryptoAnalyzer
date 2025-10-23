d03e99a444ec82852076d4fe6552180e84921b84
RodionovNikita <radzivonaumikita@gmail.com>
2025-10-23 22:27:34 +0200

    Application.java updated

--- src/main/java/com/javarush/rodionov/cryptoanalyzer/Application.java
+++ src/main/java/com/javarush/rodionov/cryptoanalyzer/Application.java
@@ -1,6 +1,24 @@
 package com.javarush.rodionov.cryptoanalyzer;
 
+import com.javarush.rodionov.cryptoanalyzer.controllers.MainController;
+import com.javarush.rodionov.cryptoanalyzer.entities.Result;
+import com.javarush.rodionov.cryptoanalyzer.exceptions.IllegalParameterException;
+
+import java.util.Arrays;
+
 public class Application {
-    public static void main( String[] args ) {
+
+    private final MainController controller;
+
+    public Application() {
+        controller = new MainController();
+    }
+
+    public Result run(String[] args) {
+        if (args.length > 0) {
+            String command = args[0];
+            String[] parameters = Arrays.copyOfRange(args, 1, args.length);
+            return controller.executeCommand(command, parameters);
+        } else throw new IllegalParameterException();
     }
 }
