3563cb9f30a59abcdcf573fc5d9f06e1595e4bea
RodionovNikita <radzivonaumikita@gmail.com>
2025-10-24 15:49:49 +0200

    created CipherUtils class

---
+++ src/main/java/com/javarush/rodionov/cryptoanalyzer/utils/CipherUtils.java
@@ -0,0 +1,30 @@
+package com.javarush.rodionov.cryptoanalyzer.utils;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public final class CipherUtils {
+
+    private CipherUtils() {}
+
+    public static Map<Character, Integer> buildSymbolMap(char[] symbols) {
+        Map<Character, Integer> indexOfChar = new HashMap<>();
+
+        for (int i = 0; i < symbols.length; i++) {
+            indexOfChar.put(symbols[i], i);
+        }
+
+        return indexOfChar;
+    }
+
+    public static char getShiftChar(char c, int key, char[] symbols, Map<Character, Integer> indexOf) {
+        Integer pos = indexOf.get(c);
+        if (pos == null) return c;
+        int newPos = (pos + key + symbols.length) % symbols.length;
+        return symbols[newPos];
+    }
+
+    public static int normalizeKey(int key, int symbolsLength) {
+        return ((key % symbolsLength) + symbolsLength) % symbolsLength;
+    }
+}
